c76b1017ceebb8cc7050c3381483cf20497e17f9
xainyuplus <2981240019@qq.com>
2025-05-30 16:13:11 +0800

    更新了加入房间后显示头像的效果

--- JS/utils/animationUtils.js
+++ JS/utils/animationUtils.js
@@ -31,17 +31,37 @@ function playersDiv(roomID) {
     roomId.textContent = roomID; 
     playersDiv.appendChild(roomId); // 将房间ID添加到 div 中
 
-    const avatarsContainer = document.createElement('div');
+    const avatarsContainer = document.createElement('div');//头像容器
+    avatarsContainer.id = 'avatarsContainer';
     avatarsContainer.style.display = 'flex';
     avatarsContainer.style.justifyContent = 'space-around'; // 头像水平分布
     avatarsContainer.style.width = '100%';
 
+    const namesContainer = document.createElement('div');//玩家名字容器
+    namesContainer.id = 'namesContainer';
+    namesContainer.style.display = 'flex';
+    namesContainer.style.justifyContent ='space-around'; // 名字水平分布
+    namesContainer.style.width = '100%';
+
+
     for(let i = 0; i < 4; i++){
-        const playerDiv = document.createElement('div'); 
-        playerDiv.classList.add('player_avatar'); // 添加一个类名，用于样式设置
-        avatarsContainer.appendChild(playerDiv); // 将新创建的 div 元素添加到容器中
+        
+        const avatar = document.createElement('img');
+        avatar.src = './assets/images/avatars/avatar0.png'; // 设置头像图片路径
+        avatar.setAttribute('data-avatar', false);
+        avatar.style.width = '60px'; // 设置头像宽度
+        avatar.style.height = '60px'; // 设置头像高度
+        avatar.style.margin = '10px'; // 设置头像之间的间距
+        avatarsContainer.appendChild(avatar); // 将头像添加到容器中
+        
+        const playerName = document.createElement('span'); // 创建一个新的 span 元素
+        playerName.textContent = '玩家' + (i + 1); // 设置 span 元素的文本内容为 '玩家1' 或 '玩家2' 等
+        playerName.style.margin = '10px'; // 设置 span 元素的边距为 '10px'
+        namesContainer.appendChild(playerName); // 将 span 元素添加到 div 中
+        
     }
     playersDiv.appendChild(avatarsContainer);
+    playersDiv.appendChild(namesContainer);
 
     const startButton = document.createElement('button');
     startButton.id = 'startButton';
